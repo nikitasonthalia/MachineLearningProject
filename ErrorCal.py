841c336bd5fae29d4feb8689540ee8c5875954b9
zhang8473 <zhang8473@gmail.com>
2016-08-29 04:38:25 -0700

    finialize the project

--- ErrorCal.py
+++ ErrorCal.py
@@ -1,4 +1,4 @@
-import csv, sys
+import csv, math
 with open("predict.csv", 'r') as f_handle:
     reader = csv.reader(f_handle, delimiter=',', quotechar='"')
     pred = [ int(x) for x in next(reader) ]
@@ -9,11 +9,13 @@ with open("MLprojectOutput/week456to7Formated/part-00000", 'r') as f_handle:
         data.append(int(row[1:-2].split(',')[-1]))
 
 errors = []
+sum_ = 0
 for i, v in enumerate(data):
     if v>0:
         errors.append(abs(v-pred[i])/v)
     else:
         errors.append(1.0 if pred[i]>0 else 0)
+    sum_ += errors[-1]**2
 errors.sort()
-print ("mean=",sum(errors)/len(pred))
+print ("mean=",math.sqrt(sum_/len(pred)))
 print ("50 Percentile=", errors[int(len(pred)*0.5)], ", 75 Percentile=", errors[int(len(pred)*0.75)] )
