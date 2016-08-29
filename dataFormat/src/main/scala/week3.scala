39a0e022cbc728b7e629a00f51175a29023f96b7
nikitasonthalia <nikitasonthalia@outlook.com>
2016-08-28 19:33:46 -0700

    Add files via upload

--- dataFormat/src/main/scala/week3.scala
+++ dataFormat/src/main/scala/week3.scala
@@ -4,14 +4,18 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 object sparkweek3 {
   def main(args: Array[String]){
-  val sc = new SparkContext(new SparkConf().setAppName("week3"));
-  val textFile = sc.textFile("/Users/vikashkamdar/Desktop/MLProject/train_week3.csv")
+  val sc = new SparkContext(new SparkConf().setAppName("week"));
+  val textFile = sc.textFile("/Users/vikashkamdar/Desktop/MLprojectOutput/train_week9.csv")
   val counts = textFile.map(line => {
     val token = line.split(",");
      val key = token(4);
-      val value = token(5) +"#"+ token(10);
-      (key,value)}).groupByKey;
-  
-  counts.coalesce(1).saveAsObjectFile("/Users/vikashkamdar/Desktop/MLProject/week3output")
+   // val value = token(1) +"#"+ token(10);
+
+     // val value = token(5) +"#"+ token(10);
+      (key,(token(1),token(2),token(5),token(6),token(7),token(8),token(9),token(10)))}).groupByKey;
+    //(key,value)}).groupByKey;
+
+
+  counts.coalesce(1).saveAsTextFile("/Users/vikashkamdar/Desktop/MLprojectOutput/week9objectoutput")
 }
   }
