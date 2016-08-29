e40ff24cc5be76bf6db17ff9443a3047ee3b64ed
zhang8473 <zhang8473@users.noreply.github.com>
2016-08-29 13:40:22 -0700

    Update DataFormat.scala

--- dataFormat/src/main/scala/DataFormat.scala
+++ dataFormat/src/main/scala/DataFormat.scala
@@ -7,7 +7,7 @@ object sparkweek3 {
   val sc = new SparkContext(new SparkConf().setAppName("week"));
   for( a <- 3 to 10){
          
- 	val textFile = sc.textFile("/MLprojectOutput/train_week"+a+".csv")
+ 	val textFile = sc.textFile("MLprojectOutput/train_week"+a+".csv")
   	val counts = textFile.map(line => {
     val token = line.split(",");
     val key = token(4);
@@ -16,7 +16,7 @@ object sparkweek3 {
      // val value = token(5) +"#"+ token(10);
       (key,(token(1),token(2),token(5),token(6),token(7),token(8),token(9),token(10)))}).groupByKey;
     //(key,value)}).groupByKey;
-	counts.coalesce(1).saveAsTextFile("/MLprojectOutput/week"+a+"objectoutput")
+	counts.coalesce(1).saveAsTextFile("MLprojectOutput/week"+a+"objectoutput")
 }
 }
   }
